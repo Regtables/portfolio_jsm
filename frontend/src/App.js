0b0f69bb0c1c52bd007138842339b79033b97f40
Regtables <reghardt7@gmail.com>
2022-03-22 12:24:16 +0200

    implitmented folder stucture and basic app structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+import { About, Header, Footer, Skills, Testimonials, Work } from './Containers'
+import { NavBar } from './Components'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className = 'app'>
+      <header>
+        <nav>
+          <NavBar />
+        </nav>
+        <Header />
       </header>
+      <main>
+      <About />
+      <Work />
+      <Skills />
+      <Testimonials />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

a451ea8f4a7f12fac7c227750f543b676527c5f0
Regtables <reghardt7@gmail.com>
2022-03-22 14:16:10 +0200

    responsive navbar menu

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import {HiMenuAlt4, HiX } from 'react-icons/hi'
+import { motion } from 'framer-motion'
 
 import './navbar.scss'
 import { images } from '../../Constants'
 
 const Navbar = () => {
+  const [toggle, setToggle ] = useState(false)
+
   return (
       <nav className = 'app__navbar'>
           <div className = 'app__navbar-logo'>
@@ -19,6 +24,24 @@ const Navbar = () => {
               ))
             }
           </ul>
+
+          <div className = 'app__navbar-menu'>
+              <HiMenuAlt4 onClick = {() => setToggle(true)}/>
+              {toggle && (
+                  <div className = 'slide-in-right'>
+                      <HiX onClick = {() => setToggle(false)}/>
+                        <ul>
+                            {
+                              ['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                                <li key = {item}>
+                                    <a href={`#${item}`} onClick = {() => setToggle(false)}>{item}</a>
+                                </li>
+                              ))
+                            }
+                        </ul>
+                  </div>
+              )}
+          </div>
       </nav>
   )
 }
